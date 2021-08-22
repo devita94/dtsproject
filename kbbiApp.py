5a52cf94b10707d3026ec5e834095eb1def2f35d
devita94 <de-dvt@live.com>
2021-08-22 16:32:11 +0700

    commit 1

--- kbbiApp.py
+++ kbbiApp.py
@@ -1,6 +1,16 @@
 import tkinter as tk
+from tkinter.constants import X
 
-root = tk.Tk()
+window = tk.Tk()
+window.title("KBBI Apps")
 
+frm_entry = tk.Frame(master=window)
+ent_keyword = tk.Entry(master=frm_entry, width=X)
+label_keyword = tk.Label(master=frm_entry, text="Cari Kata")
 
-root.mainloop()
+btn_search = tk.Button(
+    master= window,
+    text="Submit"
+)
+
+window.mainloop()
