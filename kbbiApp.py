05446efe546cc1fa42d45c754d7bbf81e19a0215
devita94 <de-dvt@live.com>
2021-08-22 16:35:17 +0700

    commit 1

--- kbbiApp.py
+++ kbbiApp.py
@@ -8,9 +8,14 @@ frm_entry = tk.Frame(master=window)
 ent_keyword = tk.Entry(master=frm_entry, width=X)
 label_keyword = tk.Label(master=frm_entry, text="Cari Kata")
 
+def find_word():
+    wordList = []
+    return ent_keyword
+
 btn_search = tk.Button(
     master= window,
-    text="Submit"
+    text="Submit",
+    command=find_word()
 )
 
 window.mainloop()
