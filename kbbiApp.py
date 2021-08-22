a6a65757b2ddc28fa29d450d9a41303aabc46e68
devita94 <de-dvt@live.com>
2021-08-22 16:42:43 +0700

    commit 2

--- kbbiApp.py
+++ kbbiApp.py
@@ -9,8 +9,17 @@ ent_keyword = tk.Entry(master=frm_entry, width=X)
 label_keyword = tk.Label(master=frm_entry, text="Cari Kata")
 
 def find_word():
-    wordList = []
-    return ent_keyword
+    worddict = {
+        "ayah" : "orang tua kandung laki-laki; bapak",
+        "ibu" : "wanita yang telah melahirkan seseorang",
+        "anak" : " orang yang berasal dari atau dilahirkan di (suatu negeri, daerah, dan sebagainya)"
+    }
+    return worddict
+
+def check_input():
+    user_input = ent_keyword.get()
+    #if user_input in find_word():
+    return user_input
 
 btn_search = tk.Button(
     master= window,
